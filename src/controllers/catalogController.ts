9b7c33d28a7e087215bdd99823993ca399e6925b
velizar-velikov <vvelikov7.22@gmail.com>
2025-01-28 15:31:28 +0200

    add car details page

--- src/controllers/catalogController.ts
+++ src/controllers/catalogController.ts
@@ -10,7 +10,13 @@ function dashboardController(req: Request, res: Response) {
     res.render('dashboard', { title: 'Cars dashboard' });
 }
 
+function detailsController(req: Request, res: Response) {
+    const { id } = req.params;
+    res.render('details', { title: id });
+}
+
 catalogRouter.get('/', homeController);
 catalogRouter.get('/cars', dashboardController);
+catalogRouter.get('/cars/:id', detailsController);
 
 export default catalogRouter;
