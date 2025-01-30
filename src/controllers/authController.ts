f998c441d2f4e873e188e5cd63ba5864e437e9eb
velizar-velikov <vvelikov7.22@gmail.com>
2025-01-30 12:01:55 +0200

    implement register and login controllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,57 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+import { validationResult } from 'express-validator';
+import { authService } from '../services/authService';
+import tokenService from '../services/tokenService';
+import { config } from '../config/config';
+import { isGuest } from '../middlewares/guards';
+import { validateInputs } from '../validations/validations';
+import { AppError } from '../utils/appError';
 
 const authRouter: Router = Router();
 
-function loginController(req: Request, res: Response) {
-    res.render('login');
-}
+const loginController = {
+    get(req: Request, res: Response) {
+        res.render('login');
+    },
+    async post(req: Request, res: Response, next: NextFunction) {
+        const { email, password } = req.body;
+        try {
+            const user = await authService.login(email, password);
+            const token = tokenService.create(user);
+            res.cookie(config.cookie_name, token, { httpOnly: true });
+            res.redirect('/');
+        } catch (error) {
+            next(new AppError('login', error, req.body));
+        }
+    },
+};
 
-function registerController(req: Request, res: Response) {
-    res.render('register');
-}
+const registerController = {
+    get(req: Request, res: Response) {
+        res.render('register');
+    },
+    async post(req: Request, res: Response, next: NextFunction) {
+        const { email, password } = req.body;
+        try {
+            const result = validationResult(req);
 
-authRouter.get('/login', loginController);
-authRouter.get('/register', registerController);
+            if (!result.isEmpty()) {
+                result.throw();
+            }
+
+            const user = await authService.register(email, password);
+            const token = tokenService.create(user);
+            res.cookie(config.cookie_name, token, { httpOnly: true });
+            res.redirect('/');
+        } catch (error) {
+            next(new AppError('register', error, req.body));
+        }
+    },
+};
+
+authRouter.get('/login', isGuest(), loginController.get);
+authRouter.post('/login', isGuest(), loginController.post);
+authRouter.get('/register', isGuest(), registerController.get);
+authRouter.post('/register', isGuest(), validateInputs('register'), registerController.post);
 
 export default authRouter;
