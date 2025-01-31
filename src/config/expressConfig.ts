e37cdad5589ce9c249f718c396a926123e8065af
velizar-velikov <vvelikov7.22@gmail.com>
2025-01-31 10:21:52 +0200

    add morgan logger

--- src/config/expressConfig.ts
+++ src/config/expressConfig.ts
@@ -1,8 +1,10 @@
 import { Application, urlencoded, static as static_ } from 'express';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import { session } from '../middlewares/session';
 
 export function expressConfig(app: Application) {
+    app.use(morgan('dev'));
     app.use(cookieParser());
     app.use(session());
     app.use('/static', static_('static'));
