103b6418ad73e188896a24951b1d943158f13f98
velizar-velikov <vvelikov7.22@gmail.com>
2025-02-01 13:31:02 +0200

    implement edit page functionality

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -36,7 +36,42 @@ const addCarController = {
     },
 };
 
+const editController = {
+    async get(req: Request, res: Response) {
+        const { id } = req.params;
+        const car = await carService.getById(id);
+
+        res.render('edit-car', { data: car });
+    },
+    async post(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        try {
+            const result = validationResult(req);
+
+            if (!result.isEmpty()) {
+                result.throw();
+            }
+
+            await carService.edit(id, req.body);
+            res.redirect(`/cars/${id}`);
+        } catch (error) {
+            const data = {
+                ...req.body,
+                fuel: {
+                    Petrol: req.body.fuel == 'Petrol',
+                    Diesel: req.body.fuel == 'Diesel',
+                    lpg: req.body.fuel == 'LPG',
+                    electric: req.body.fuel == 'Electric',
+                },
+            };
+            next(new AppError('add-car', error, data));
+        }
+    },
+};
+
 carRouter.get('/add-car', isUser(), addCarController.get);
 carRouter.post('/add-car', isUser(), validateInputs('car'), addCarController.post);
+carRouter.get('/cars/:id/edit', isUser(), editController.get);
+carRouter.post('/cars/:id/edit', isUser(), validateInputs('car'), editController.post);
 
 export default carRouter;
